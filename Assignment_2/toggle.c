0efbcd0059947eac2bc43b55d4db068f2ad598bd
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-02-27 17:27:06 -0500

    Edited code and transferred complete files from BBB

--- Assignment_2/toggle.c
+++ Assignment_2/toggle.c
@@ -12,37 +12,39 @@ int main(){
     /*File Setup Section*/
 
     FILE *led_file = fopen(LED_PATH, "r+");
-    FILE *time_diff = fopen("time_diff_file.txt", 'a');
+    FILE *time_diff = fopen("time_diff_file.txt", "a");
 
-    if (led_file == NULL){
+    if (led_file == NULL || time_diff == NULL){
         printf("Error opening file");
     }
 
 
     /*Main Section - Toggling LED on/off */
 
-    clock_gettime(CLOCK_MONOTONIC, &start); //store at pointer for start
+    for (int i=0;i<300;i++){
 
-    for (int i=0;i<10;i++){
+        clock_gettime(CLOCK_MONOTONIC, &start); //store at pointer for start
 
         usleep(1000000); //Added delay
 
         char value;
         fread(&value, 1, 1, led_file);
-        
+        fseek(led_file, 0, SEEK_SET);
+
         if (value == '0'){
             fwrite("1", sizeof(char), 1, led_file);
         }
-        else{
+        else {
             fwrite("0", sizeof(char), 1, led_file);
         }
+        fseek(led_file, 0, SEEK_SET);
+
+        clock_gettime(CLOCK_MONOTONIC, &end); //store at poitner for end timestamp
+
+        /*Writing to file*/
+        duration = end.tv_nsec - start.tv_nsec;
+        fprintf(time_diff, "%ld \n", duration);
     }
-
-    clock_gettime(CLOCK_MONOTONIC, &end); //store at poitner for end timestamp
-
-    /*Writing to file*/
-    duration = end.tv_nsec - start.tv_nsec;
-    fprintf(time_diff, "%ld \n", duration);
 
     /*Closing files*/
     fclose(time_diff);
