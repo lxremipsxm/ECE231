94f24899dba00195a9eb804eb6aa58e63be7f922
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-04-10 18:22:16 -0400

    Added final functionalities, recorded video

--- Assignment_8.1/lab8_1.c
+++ Assignment_8.1/lab8_1.c
@@ -1,11 +1,15 @@
 /* 
 Siddharth Vasudevan, ECE231 SP25 Lab 8.1
 
+Description: This code enables the ATmega to take input from three buttons and flash two leds
+in different permuations, while preventing simultaneous button presses.
+
 Connections:
 PB5 - Physical Pin D11: Green LED Anode
 PB4 - Physical Pin D12: Red LED Anode
-PC0 - Physical Pin A0: Left Button (For red)
-PC1 - Physical Pin A1: Right Button (For green)
+PC0 - Physical Pin A0: Leftmost Button (For red)
+PC1 - Physical Pin A1: Middle Button (For green)
+PC2 - Physical Pin A2: Rightmost Button (for simultaneous activation)
 
 5Hz rate should flash once every 0.2s (200ms)
 */
@@ -16,13 +20,13 @@ PC1 - Physical Pin A1: Right Button (For green)
 
 int main(void){
 
-    //Set port C pins c0 and c1 to input
-    DDRC&= ~(1 <<PC0);
-    DDRC&= ~(1 <<PC1);
+    //Set port C pins c0, c1, and c2 as input at once
+    DDRC&= ~(1 <<0b00000111); 
 
     //Enable pull-up resistors on butons
     PORTC |= (1 << PC0);  
-    PORTC |= (1 << PC1);  
+    PORTC |= (1 << PC1);
+    PORTC |= (1 << PC2);  
 
     //outputs on port b
     DDRB = 0xFF;
@@ -36,16 +40,42 @@ int main(void){
             PORTB &= ~(1<<PB4); //bitwise and on bit 4
             _delay_ms(200);
 
+            while (!(PINC & (1<<PC1)) || !(PINC & (1<<PC2))){
+                PORTB &= ~(1<<PB4);
+            }
+
+
         }
 
         while (!(PINC & (1<<PC1))){ //while pc1 is high
 
-            PORTB |= (1<<PB3); //bitwise or on bit 5
+            PORTB |= (1<<PB3); //bitwise or on bit 3
             _delay_ms(200);
-            PORTB &= ~(1<<PB3); //bitwise and on bit 5
+            PORTB &= ~(1<<PB3); //bitwise and on bit 3
             _delay_ms(200);
 
+            while (!(PINC & (1<<PC0)) || !(PINC & (1<<PC2))){
+                PORTB &= ~(1<<PB3);
+            }
+
         }
+
+        while (!(PINC & (1<<PC2))){ //while pc2 is high
+
+            PORTB |= (1<<PB3); 
+            PORTB |= (1<<PB4);
+            _delay_ms(200);
+            PORTB &= ~(1<<PB3);
+            PORTB &= ~(1<<PB4);
+            _delay_ms(200);
+
+            while (!(PINC & (1<<PC0)) || !(PINC & (1<<PC1))){
+                PORTB &= ~(1<<PB3);
+                PORTB &= ~(1<<PB4);
+            }
+
+        }
+
     }
 
     return 0;
