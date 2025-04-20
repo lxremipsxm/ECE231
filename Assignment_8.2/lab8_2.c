348dd05538de0d87743fba176b323242a8c65939
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-04-20 14:31:58 -0400

    Added conversion to temperature and between C and F

--- Assignment_8.2/lab8_2.c
+++ Assignment_8.2/lab8_2.c
@@ -13,28 +13,64 @@ int main(void){
 
     OLED_Init();
     unsigned int digiValue;
-    DDRC = 0x00; //make port c all input
+    DDRC = 0x00; //make port c all input 
+    PORTC |= (1<<PC1); //enable pull up on button pin PC1
 
     uart_init();
     adc_init(); //default channel (PC0)
-
+    int c_flag = 1; //celsius flag (supposed to represent bool)
 
     while(1){
 
-        char buffer[10];
+        //toggle celsius flag
+        if (c_flag == 1 && !(PINC & (1<<PC1))){
+            c_flag = 0; 
+        }
+        
+        else if (c_flag == 0 && !(PINC & (1<<PC1))){
+            c_flag = 1;
+        }
+
         digiValue = get_adc();
 
-        send_string("Digitized Value: ");
-        send_string(itoa(digiValue, buffer, 10)); //convert to string
-        uart_send(13); //carriage return
-        uart_send(10); //end line feed
+        
+        float temp_c = (((digiValue/1023.0) * 5.0)-0.5)*100 ; //scaling to convert to celsius, 10mV/C
+        float temp_f = (temp_c * 9/5) + 32;
 
         OLED_GoToLine(1);
         OLED_DisplayString("Temperature");
-        
-        OLED_GoToLine(5);    
-        OLED_DisplayNumber(10, digiValue, 4);
 
-        _delay_ms(1000);
+        uart_send(13); //carriage return
+        uart_send(10); //end line feed
+
+        char output[32];
+        char temp_str[10];
+
+        if (c_flag == 1) {
+            
+            dtostrf(temp_c, 4,1, temp_str);
+            sprintf(output, "Temperature: %s C", temp_str);
+            
+            OLED_GoToLine(5);    
+            OLED_DisplayString(temp_str);
+            OLED_DisplayString(" C");
+
+        }
+
+        else {
+            
+            dtostrf(temp_f, 4,1, temp_str);
+            sprintf(output, "Temperature: %s F", temp_str);
+            
+            OLED_GoToLine(5);    
+            OLED_DisplayString(temp_str);
+            OLED_DisplayString(" F");
+
+        }
+
+        send_string(output);
+
+        _delay_ms(300);
+        
     }
 }
